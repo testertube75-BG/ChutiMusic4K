31ce8c3781ad35c26dea8c4e56f732fe09a41971
BG <testertube75@gmail.com>
2026-05-17 23:49:48 +0530

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -85,20 +85,22 @@ def ytdlp_extract(query: str, video: bool = False) -> StreamInfo:
     source = query if YOUTUBE_OR_URL_RE.match(query) else f"ytsearch1:{query}"
 
     ydl_opts = {
-    "format": "best",
+    "format": "worst",
+    "source_address": "0.0.0.0",
 
     "quiet": True,
     "no_warnings": True,
     "skip_download": True,
     "noplaylist": True,
-    "default_search": "ytsearch1",
+    "default_search": "ytsearch",
+
     "cookiefile": "cookies.txt",
 
     "extractor_args": {
         "youtube": {
             "player_client": ["android"]
         }
-    },
+    }
 }
     
     with YoutubeDL(ydl_opts) as ydl:
